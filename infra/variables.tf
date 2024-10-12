13fd736df08567ee9f5787f5a28438994deabae1
Tiago Paraizo <tiagoparaizo@MacBook-Air-de-Tiago.local>
2024-10-12 18:44:42 -0300

    feat: first version of template

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,18 @@
-variable "ecr_image_tag" {
-  type = string
-}
+################################################
+# [LitPipes] ECR Variables
+# This variables could be used when the repository use ECR as container repository
+# If this repository use the ECR as container repository then these variables will be automatically completed
+################################################
 
-variable "ecr_image_repository" {
-  type = string
-}
+#variable "ecr_image_tag" {
+#  type = string
+#}
+
+#variable "ecr_image_repository" {
+#  type = string
+#}
+
+################################################
+# Your Variables
+# Declare your variables below
+################################################
